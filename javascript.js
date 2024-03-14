ef00284b561352a92c4c2f65021b21f5e4695c93
oldmanfromscene24 <giancarlo.spiller@gmail.com>
2024-03-14 22:30:38 +0100

    feat(html, css, js): add grid
    Add code for Etch a sketch The Odin Project. Added grid of divs.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function createGrid() {
+  const container = document.querySelector(".container");
+  for (let i = 0; i < 16*16; i++) {
+    div = document.createElement("div");
+    container.appendChild(div);
+  }
+};
