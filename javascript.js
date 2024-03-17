75838fbb520cf697b8494b3a4db0e7a4644332bf
oldmanfromscene24 <giancarlo.spiller@gmail.com>
2024-03-17 17:53:55 +0100

    feat(js): add createColor function
    Added a function to create color, removed default color in CSS.

--- javascript.js
+++ javascript.js
@@ -2,6 +2,15 @@ function createGrid() {
   const container = document.querySelector(".container");
   for (let i = 0; i < 16*16; i++) {
     div = document.createElement("div");
+    div.style.backgroundColor = createColor();
     container.appendChild(div);
   }
-};
+}
+
+function createColor() {
+  red = Math.floor(Math.random() * 255 + 1);
+  green = Math.floor(Math.random() * 255 + 1);
+  blue = Math.floor(Math.random() * 255 + 1);
+  rgbColor = "rgb(" + red + "," + green + "," + blue + ")";
+  return rgbColor;
+}
