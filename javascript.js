c75a9d505ed960dc66dde86641a2d15fba6baa1a
oldmanfromscene24 <giancarlo.spiller@gmail.com>
2024-03-18 20:28:00 +0100

    feat(project): first full version
    Corrected CSS, now it's ok with all grids, implemented progressive
    darkening, refactored some functions to call only needed arguments.

--- javascript.js
+++ javascript.js
@@ -2,46 +2,43 @@ const container = document.querySelector(".container");
 const button = document.querySelector("button");
 for (let i = 0; i < 16 * 16; i++) {
   let div = document.createElement("div");
-  div.classList.add("0"); //class used to track how many times this div has been mouseover
+  div.classList.add("0"); // class counts how many times this div has been mouseover
   div.id = i;
   container.appendChild(div);
 }
 container.addEventListener("mouseover", (e) => {
-  const rgbValuesArray = e.target.style.backgroundColor.slice(4, -1).split(",");
   if (e.target.className < 10)
-    e.target.style.backgroundColor = createColor(
-      e.target.className,
-      e.target.id,
-      rgbValuesArray
-    );
+    e.target.style.backgroundColor = createColor(e.target.id);
 });
 
 button.addEventListener("click", changeGrid);
 
-function createColor(hoverNum, id, rgb) {
-  let rgbValues;
-  if (hoverNum == 0) {
-    rgbValues = createRgbValues();
-    let hovered = document.getElementById(id);
-    hovered.className = parseInt(hovered.className) + 1;
-    console.log(hovered.className);
-  } else rgbValues = darkTen(hoverNum, id, rgb);
+function createColor(id) {
+  let hovered = document.getElementById(id);
+  hovered.className = parseInt(hovered.className) + 1; // mouseover + 1
+  let rgbValues = createRgbValues(parseInt(hovered.className), id);
   const rgbColor =
     "rgb(" + rgbValues[0] + "," + rgbValues[1] + "," + rgbValues[2] + ")";
   return rgbColor;
 }
 
-function createRgbValues() {
-  const red = Math.floor(Math.random() * 255 + 1);
-  const green = Math.floor(Math.random() * 255 + 1);
-  const blue = Math.floor(Math.random() * 255 + 1);
-  return [red, green, blue];
-}
-
-function darkTen(hoverNum, id, rgb) {
-  let hovered = document.getElementById(id);
-  hovered.className = parseInt(hovered.className) + 1;
-  return [0, 0, 0];
+function createRgbValues(hoverNum, id) {
+  if (hoverNum === 10) return [0, 0, 0];
+  else if (hoverNum > 1) {
+    const rgbValuesArray = document
+      .getElementById(id)
+      .style.backgroundColor.slice(4, -1)
+      .split(",");
+    const darkRed = rgbValuesArray[0] - (rgbValuesArray[0] / 10) * hoverNum;
+    const darkGreen = rgbValuesArray[1] - (rgbValuesArray[1] / 10) * hoverNum;
+    const darkBlue = rgbValuesArray[2] - (rgbValuesArray[2] / 10) * hoverNum;
+    return [darkRed, darkGreen, darkBlue];
+  } else
+    return [
+      Math.floor(Math.random() * 255 + 1), //red
+      Math.floor(Math.random() * 255 + 1), //green
+      Math.floor(Math.random() * 255 + 1), //blue
+    ];
 }
 
 function changeGrid() {
@@ -58,6 +55,8 @@ function changeGrid() {
     let div = document.createElement("div");
     div.style.width = divSize;
     div.style.height = divSize;
+    div.classList.add("0");
+    div.id = i;
     container.appendChild(div);
   }
 }
